88fe2fc50833e0419b34b7da18a1c6cb57dd46fe
gsy1234567 <geshy@shanghaitech.edu.cn>
2024-06-18 14:48:00 +0800

    add grid.py

--- grid.py
+++ grid.py
@@ -3,68 +3,88 @@ import numpy as np
 import math
 import open3d as o3d
 
+class Grid3d:
+    def __init__(self, min:tuple[float, float, float], max:tuple[float, float, float], resolution:tuple[int, int, int]) -> None:
+
+        '''@resolution:在长、宽、高上grid的数量'''
+        self.min:np.ndarray = np.array(min)
+        self.max:np.ndarray = np.array(max)
+        self.resolution = np.array(resolution)
 
 
+    def __get_delta(self)->np.ndarray:
+        delta = self.max - self.min
+        delta /= self.resolution
+        return delta
 
-class Grid3d : 
-    def __init__(self, xmin:float, xmax:float, ymin:float, ymax:float, zmin:float, zmax:float, x_res:int, y_res:int, z_res:int) -> None:
-        self.min = np.array([xmin, ymin, zmin])
-        self.max = np.array([xmax, ymax, zmax])
-        assert(x_res >= 2 and y_res >= 2 and z_res >= 2)
-        self.res = np.array([x_res, y_res, z_res])
-        
-    def get_delta(self, dim:int)->float:
-        assert(0 <= dim < 3)
-        return (self.max[dim] - self.min[dim]) / (self.res[dim] - 1)
+    def __idx_check(self, idx3d:tuple[int, int, int])->None:
+        assert(0 <= idx3d[0] < self.resolution[0])
+        assert(0 <= idx3d[1] < self.resolution[1])
+        assert(0 <= idx3d[2] < self.resolution[2])
 
-    def get_deltas(self)->list[float, float, float]:
+    def get_point_idx3d(self, pos:tuple[float, float, float])->tuple[int, int, int]:
+        pos = np.array(pos)
+        idx3d = (pos - self.min) / self.__get_delta()
+        idx3d = np.floor(idx3d)
+        idx3d = np.int32(idx3d)
+        self.__idx_check(idx3d)
+        return idx3d
 
-        return (self.max - self.min) / (self.res - 1)
+    def get_point_idx1d(self, pos:tuple[float, float, float])->tuple[int]:
+        idx3d = self.get_point_idx3d(pos)
+        return idx3d[0] + self.resolution[0] * (idx3d[1] + self.resolution[1] * idx3d[2])
 
-    def get_1d_idx(self, pos:float, dim:int)->tuple[int, int]:
-        assert(0 <= dim < 3)
-        assert(self.min[dim] <= pos <= self.max[dim]) 
+    def idx1d_to_idx3d(self, idx1d:int)->tuple[int, int, int]:
+        x_idx = int(idx1d % self.resolution[0])
+        y_idx = int((idx1d / self.resolution[0]) % self.resolution[1])
+        z_idx = int((idx1d / self.resolution[0]) / self.resolution[2])
+        return (x_idx, y_idx, z_idx)
 
-        delta = self.get_delta(dim)
-        ceil = math.ceil(pos / delta)
-        floor = math.floor(pos / delta)
-        return (ceil, floor)
+
     
-    def get_3d_idx(self, pos3d)->tuple[tuple[int, int], tuple[int, int] , tuple[int, int]]:
-        return (self.get_1d_idx(pos3d[0], 0), self.get_1d_idx(pos3d[1], 1), self.get_1d_idx(pos3d[2], 2))
-    
-    def get_pos1d(self, idx:int, dim:int)->float:
-        assert(0 <= dim < 3)
-        assert(0 <= idx < self.res[dim])
-        return self.min[dim] + idx * self.get_delta(dim)
-        
-    
-    def get_pos3d(self, idx3d)->tuple[float, float, float]:
-        return (self.get_pos1d(idx3d[0], 0), self.get_pos1d(idx3d[1], 1), self.get_pos1d(idx3d[2], 2))
+    def get_idx3d_base_func_idx(self, idx3d:tuple[int, int, int])->tuple[int, int, int, int, int, int]:
+        '''@return 
+            ret[0]:xmin base function idx
+            ret[1]:xmax base function idx
+            ret[2]:ymin base function idx
+            ret[3]:ymax base function idx
+            ret[4]:zmin base function idx
+            ret[5]:zmax base function idx
+        '''
+        x_idx, y_idx, z_idx = idx3d
 
-    def vis_all_points(self)->None:
+        x_min_base = x_idx * self.resolution[1] * self.resolution[2]
+        x_max_base = (x_idx + 1) * self.resolution[1] * self.resolution[2]
+        x_offset = y_idx * self.resolution[2] + z_idx
 
-        points = np.zeros([self.res[0] * self.res[1] * self.res[2], 3])
+        y_min_base = y_idx * self.resolution[0] * self.resolution[2]
+        y_max_base = (y_idx + 1) * self.resolution[0] * self.resolution[2]
+        y_offset = z_idx * self.resolution[0] + x_idx
 
-        delta_x, delta_y, delta_z = self.get_delta(0), self.get_delta(1), self.get_delta(2)
+        z_min_base = z_idx * self.resolution[0] * self.resolution[1]
+        z_max_base = (z_idx + 1) * self.resolution[0] * self.resolution[1]
+        z_offset = x_idx * self.resolution[1] + y_idx
 
-        for z_idx in range(self.res[2]):
-            z_pos = self.min[2] + delta_z * z_idx
-            for y_idx in range(self.res[1]):
-                y_pos = self.min[1] + delta_y * y_idx
-                for x_idx in range(self.res[0]):
-                    x_pos = self.min[0] + delta_x * x_idx
-                    global_idx = x_idx + self.res[0] * (y_idx + self.res[1] * z_idx)
-                    points[global_idx, 0] = x_pos
-                    points[global_idx, 1] = y_pos
-                    points[global_idx, 2] = z_pos
+        #local idx to global
+        y_offset += self.resolution[1] * self.resolution[2] * (self.resolution[0] + 1)
+        z_offset += self.resolution[1] * self.resolution[2] * (self.resolution[0] + 1)
+        z_offset += self.resolution[0] * self.resolution[2] * (self.resolution[1] + 1)
 
-        point_cloud = o3d.geometry.PointCloud()
-        point_cloud.points = o3d.utility.Vector3dVector(points)
-        o3d.visualization.draw_geometries([point_cloud])
+        return (x_min_base + x_offset, x_max_base + x_offset, y_min_base + y_offset, y_max_base + y_offset, z_min_base + z_offset, z_max_base + z_offset)
+
+
+    def get_idx1d_base_func_idxs(self, idx1d:int)->tuple[int, int, int, int, int, int, int]:
+        return self.get_idx3d_base_func_idx(self.idx1d_to_idx3d(idx1d))
+
+    def get_point_base_func_idxs(self, pos:tuple[float, float, float])->tuple[int, int, int, int, int, int]:
+        return self.get_idx3d_base_func_idx(self.get_point_idx3d(pos))
+
 
 if __name__ == "__main__":
-    grid3d = Grid3d(-0.4, 0.4, -0.2, 0.2, -0.05, 0.05, 17, 9, 3)
-    grid3d.vis_all_points()
-    pass
+    grid3d = Grid3d((0.0,0.0,0.0), (1.0, 1.0, 1.0), (1, 1, 1))
+    print(grid3d.get_idx1d_base_func_idxs(0))
+
+
+
+    
 
